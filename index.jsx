2a0b60fb7682d2c4c927972a43f29a325e6d9266
Reachpilot <reachpilot.ceo@gmail.com>
2025-04-22 20:50:55 +0200

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>Welcome to ReachPilot 👋</h1>
-      <p>Your outreach automation starts here.</p>
-    </div>
-  );
-}
+export default function Home() { return <h1>Welcome to ReachPilot</h1>; }
