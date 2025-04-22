ad20104de7926d13432057aa823249accd60affa
Reachpilot <reachpilot.ceo@gmail.com>
2025-04-22 20:45:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-export default function Home() { return <h1>Welcome to ReachPilot</h1>; }
+export default function Home() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1>Welcome to ReachPilot 👋</h1>
+      <p>Your outreach automation starts here.</p>
+    </div>
+  );
+}
